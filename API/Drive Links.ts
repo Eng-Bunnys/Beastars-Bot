1abf6c0893213b78d3095ac724a52b92cbbc0340
Bunnys <78443317+Eng-Bunnys@users.noreply.github.com>
2023-10-25 20:06:32 +0300

    Beastars Bot V2

---
+++ API/Drive Links.ts
@@ -0,0 +1,20 @@
+/**
+ * Beastars Bot 
+ * Written by .bunnys => 333644367539470337
+ * Need help? Support Server: https://discord.gg/yrM7fhgNBW
+ * GBF Bot: https://discord.com/api/oauth2/authorize?client_id=795361755223556116&permissions=1642787765494&scope=bot%20applications.commands
+ 
+  Beastars Bot written under GBF
+
+  Drive Links
+*/
+export enum DriveLinksNames {
+  "HCS  Beastars" = "https://drive.google.com/drive/folders/1w4lWdeH9VEK-CBN3PNCdKMLqXlRAoYxZ?usp=sharing",
+  "HCS Beast Complex" = "https://drive.google.com/drive/folders/1CnFqvLlhuJFbTrBSQx6ebFUtiuET1aQT",
+  "Hybridgumi Beastars" = "https://drive.google.com/drive/folders/14o2LSwOFZrtAYQ8q6-PVEe5JKd5ZJngq?usp=drive_link",
+  "Hybridgumi Beast Complex" = "https://drive.google.com/drive/folders/1GNnfSQlNBj7A7tDxbQtMFJyCMYOsyXJQ?usp=drive_link",
+  "Viz  Beastars" = "https://drive.google.com/drive/folders/1eoxePiCfkIPpQmYg57q96ZjyoKoAoPBF?usp=drive_link",
+  "Viz  Beast complex" = "https://drive.google.com/drive/folders/11AzP4QNZ_pNeFH-ezgHuHGcPAnu4Z68z?usp=drive_link",
+  "Raw Beastars" = "https://drive.google.com/drive/folders/11RZwgfS6mZ0R5fBwm--tT5rxkXHktBkA?usp=drive_link",
+  "Raw Beast Complex" = "https://drive.google.com/drive/folders/1T9_kFiMZA9LH8uBXgdo0mkSkdB1V09US?usp=drive_link"
+}
